4b2aaea1f46842c999035bd4af30f5724f16328b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-19 20:59:54 +0530

    Update Nav.jsx

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='h-12 w-full'>
-        <h1 className="logo-title">GoBidly</h1>
+    <div className='py-4 md:px-32 px-8 w-full bg-[#FF5722]'>
+        <div className="flex justify-between">
+            <div className="flex ">
+                <h1 className="logo-title text-white text-3xl">GoBidly</h1>
+                <div className="pl-4 text-white">
+                    <h1 className="font-semibold">Auction</h1>
+                    <p className="">Platform</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
