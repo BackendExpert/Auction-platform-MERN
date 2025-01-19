fee06d7902e3503e5f2616c2ebb85f3765c570e1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-19 21:12:53 +0530

    Update Nav.jsx

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { TiThMenu } from "react-icons/ti";
+import { IoClose } from "react-icons/io5";
+import React, { useState } from 'react';
 
 const Nav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const handleMenuClick = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
   return (
     <div className='py-4 md:px-32 px-8 w-full bg-[#FF5722]'>
         <div className="flex justify-between">
@@ -11,6 +18,28 @@ const Nav = () => {
                     <p className="">Platform</p>
                 </div>
             </div>
+            <div className="">
+                <div className="flex items-center mt-3">
+                    {isMenuOpen ? (
+                        <IoClose onClick={handleMenuClick} className='cursor-pointer h-6 w-auto fill-white' />
+                    ) : (
+                        <TiThMenu onClick={handleMenuClick} className='cursor-pointer h-6 w-auto fill-white' />
+                    )}
+                </div>
+            </div>
+        </div>
+
+        <div
+          className={`text-white mt-8 absolute top-20 right-0 w-64 bg-[#FF5722] shadow-lg rounded-lg p-4 transform transition-transform duration-300 ease-in-out ${
+            isMenuOpen ? 'translate-x-0' : 'translate-x-full'
+          }`}
+        >
+          <div className="flex flex-col">
+            <h1 className="mx-4 my-2 cursor-pointer"><a href="/">Home</a></h1>
+            <h1 className="mx-4 my-2 cursor-pointer">How Works</h1>
+            <h1 className="mx-4 my-2 cursor-pointer">About Us</h1>
+            <h1 className="mx-4 my-2 cursor-pointer"><a href="signin">Login</a></h1>
+          </div>
         </div>
     </div>
   )
