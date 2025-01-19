e6dd14125296c45d4c0da87751611c07c8fe454a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-19 20:38:00 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,59 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import Nav from "./components/Nav/Nav";
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer/Footer";
+
 export default function App() {
+  const [showNavBar, setShowNavBar] = useState(true);
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY && currentScrollY > 50) {
+
+      setShowNavBar(false);
+    } else {
+
+      setShowNavBar(true);
+    }
+    setIsTopOfPage(currentScrollY === 0);
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  const shouldShowNavBar = !location.pathname.startsWith("/Dashboard");
+  const shouldShowFooter = !location.pathname.startsWith("/Dashboard");
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+        {shouldShowNavBar && (
+          <div
+          className={`fixed top-0 w-full z-50 transition-transform duration-300 ${
+            showNavBar ? "translate-y-0" : "-translate-y-full"
+          } ${
+            isTopOfPage
+              ? "bg-transparent transition-colors duration-500"
+              : "bg-[#7B3F00] shadow-md transition-colors duration-500"
+          }`}
+          >
+            <Nav />
+          </div>
+        )}
+      <Routes>
+        <Route path="/" element={<HomePage /> } />
+      </Routes>
+      {shouldShowFooter && <Footer />}
+    </BrowserRouter>
   )
 }
