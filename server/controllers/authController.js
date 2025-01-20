632bee819648f31206acef2624005f7b39e991cd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-20 13:14:48 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const AuthController = {
     siginup: async(req, res) => {
@@ -37,6 +38,35 @@ const AuthController = {
                 else{
                     return res.json({Error: "Internal Server Error while Creating User"})
                 }
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+    },
+
+    signin: async(req, res) => {
+        try{
+            const {
+                email,
+                password
+            } = req.body
+
+            const checkuser = await User.findOne({ email: email })
+
+            if(checkuser){
+                const checkpass = await bcrypt.compare(password, checkuser.password)
+
+                if(checkpass){
+                    const token = jwt.sign({ id: checkuser._id, role:checkuser.role }, process.env.JWT_SECRET);
+                    return res.json({ Status: "Success", Result: checkuser, Token: token })
+                }
+                else{
+                    return res.json({ Error: 'Password Not Match'})
+                }
+            }
+            else{
+                return res.json({ Error: "No user found..."})
             }
         }
         catch(err){
