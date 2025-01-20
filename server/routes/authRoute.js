251ec72836979a6ce86156bc040d4df305f84b87
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-20 13:16:35 +0530

    Update authRoute.js

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -4,5 +4,6 @@ const AuthController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', AuthController.siginup)
+router.post('/signin', AuthController.signin)
 
 module.exports = router;
