08d6a26276f7c5a0934762213f57b64a1f08aa40
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-20 18:40:23 +0530

    Update PrivateRoute.jsx

--- client/src/components/Auth/PrivateRoute.jsx
+++ client/src/components/Auth/PrivateRoute.jsx
@@ -3,6 +3,7 @@ import { Navigate } from 'react-router-dom';
 // Function to check if user is authenticated
 const isAuthenticated = () => {
   return !!localStorage.getItem('login'); // Check for token in localStorage
+  window.location.reload()
 };
 
 const PrivateRoute = ({ element }) => {
