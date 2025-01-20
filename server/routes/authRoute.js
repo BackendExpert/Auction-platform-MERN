8266058f7ef13e57c708fd03b96d3b019a2e4b23
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-20 13:50:03 +0530

    Update authRoute.js

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/signup', AuthController.siginup)
 router.post('/signin', AuthController.signin)
 router.post('/forgetpass', AuthController.forgetpass)
+router.post('/resetpass', AuthController.forgetpass)
 
 module.exports = router;
