64b0d0dba11b7c19e8b8bf568eff82fe5d95f37c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-20 11:57:51 +0530

    Update SignUp.jsx

--- client/src/pages/AuthPages/SignUp.jsx
+++ client/src/pages/AuthPages/SignUp.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='px-32 py-24'>
+        <div className="md:grid grid-cols-3 gap-3 mt-16">
+            <div className="bg-gray-100/20 p-4 shadow-md rounded-md">
+                <h1 className="text-[#FF5722] font-semibold uppercase text-center">Create New Account</h1>
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
