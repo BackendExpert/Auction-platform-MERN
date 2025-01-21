099daf2bc56efd3632c538ff7899bcb9ac7f2d33
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-21 07:12:26 +0530

    Update PageNotFound.jsx

--- client/src/components/ErrorPages/PageNotFound.jsx
+++ client/src/components/ErrorPages/PageNotFound.jsx
@@ -4,21 +4,40 @@ import DefultBtn from '../Buttons/DefultBtn'
 import { Link } from 'react-router-dom'
 
 const PageNotFound = () => {
+    const isDashbaord = location.pathname.startsWith("/Dashboard");
   return (
     <div className='my-32 md:px-32 px-4'>
         <center>
             <img src={PageNotFoundImg} alt="" className='h-64 w-auto'/>
 
             <h1 className="mt-4 text-2xl">Error: </h1>
-            <p className="text-xl mt-4"><span className='text-[#FF5722]'>404</span> : Page Not Found</p>
+            <p className="text-xl mt-4"><span className='text-[#FF5722]'>404</span> : { isDashbaord ? "Dashboard" : "" } Page Not Found</p>
 
             <div className="my-4">
-              <Link to={'/'}>
-                <DefultBtn 
-                  type={'button'}
-                  btnvalue={'Back to HomePage'}
-                />
-              </Link>
+              {
+                (() => {
+                  if(isDashbaord){
+                    return (
+                      <Link to={'/Dashboard/Home'}>
+                        <DefultBtn 
+                          type={'button'}
+                          btnvalue={'Back to Dashboard'}
+                        />
+                      </Link>
+                    )
+                  }else{
+                    return (
+                      <Link to={'/'}>
+                        <DefultBtn 
+                          type={'button'}
+                          btnvalue={'Back to HomePage'}
+                        />
+                      </Link>
+                    )
+                  }
+                })()
+              }
+
             </div>
         </center>
     </div>
