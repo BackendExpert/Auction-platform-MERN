c21189551571143290e60e38e873120c5ece3d52
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-31 11:04:56 +0530

    Update auctionRoute.js

--- server/routes/auctionRoute.js
+++ server/routes/auctionRoute.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/createauction/:user', auctionController.createauction)
 router.get('/allauction', auctionController.allAuction)
 router.get('/viewauction/:id', auctionController.getbyidAuction)
+router.post('/updateAuction/:id', auctionController.updateAuction)
+router.post('/auctionupdateStatus/:id', auctionController.auctionupdateStatus)
 
 module.exports = router;
