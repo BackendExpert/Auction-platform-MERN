7a527ec3fa07e9f45f9b8a882fa564f3c52fa2de
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-04 09:52:03 +0530

    Update DashHome.jsx

--- client/src/pages/DashBoard/DashHome.jsx
+++ client/src/pages/DashBoard/DashHome.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import secureLocalStorage from 'react-secure-storage'
+
 
 const DashHome = () => {
-  return (
-    <div>DashHome</div>
-  )
+    const RoleUser = secureLocalStorage.getItem('loginR')
+    const EmailUser = secureLocalStorage.getItem('loginE')
+
+    if(RoleUser === "admin"){
+        return (
+          <div>
+              admin
+          </div>
+        )
+    }
+    else if(RoleUser === "moderator"){
+        return (
+          <div>
+
+          </div>
+        )
+    }
+    else if(RoleUser === "seller"){
+        return (
+          <div>
+
+          </div>
+        )
+    }
+    else if(RoleUser === "buyer"){
+        return (
+          <div>
+
+          </div>
+        )      
+    }
 }
 
 export default DashHome
