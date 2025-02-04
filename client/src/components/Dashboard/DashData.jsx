4587c4a9db3ebd2c50cadb9661c1ceac9a1ec688
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-04 10:18:17 +0530

    Update DashData.jsx

--- client/src/components/Dashboard/DashData.jsx
+++ client/src/components/Dashboard/DashData.jsx
@@ -1,26 +1,31 @@
+import { FaPeopleGroup } from "react-icons/fa6";
+import { RiAuctionFill, RiErrorWarningFill } from "react-icons/ri";
+import { BsCashStack } from "react-icons/bs";
+
+
 const admindata = [
     {
         id: 1,
         name: 'User',
-        icon: '',
+        icon: FaPeopleGroup,
         value: 500
     },
     {
         id: 2,
         name: 'Auction',
-        icon: '',
+        icon: RiAuctionFill,
         value: 500
     },
     {
         id: 3,
         name: 'Reported Auction',
-        icon: '',
+        icon: RiErrorWarningFill,
         value: 500
     },
     {
         id: 4,
         name: 'Pltform Income',
-        icon: '',
+        icon: BsCashStack,
         value: 500
     },
 ]
