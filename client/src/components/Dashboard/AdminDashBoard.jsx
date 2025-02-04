179dce585d3d9fc3e03c38baa0709cbb83d3dee6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-04 10:15:44 +0530

    Update AdminDashBoard.jsx

--- client/src/components/Dashboard/AdminDashBoard.jsx
+++ client/src/components/Dashboard/AdminDashBoard.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { admindata } from './DashData'
 
 const AdminDashBoard = () => {
   return (
-    <div>AdminDashBoard</div>
+    <div>
+        <h1 className="uppercase text-[#FF5722] font-semibold text-xl">admin dashboard</h1>
+
+        <div className="">
+            <div className="grid xl:grid-cols-4 gap-4">
+                {
+                    admindata.map((data, index) => {
+                        return (
+                            <div className="" key={index}>
+                                <h1 className="">{data.name}</h1>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+    </div>
   )
 }
 
