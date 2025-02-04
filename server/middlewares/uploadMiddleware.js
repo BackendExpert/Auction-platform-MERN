d8739d05750d07a950f301e1c5a88486702d45b9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-04 08:34:21 +0530

    Create uploadMiddleware.js

---
+++ server/middlewares/uploadMiddleware.js
@@ -0,0 +1,32 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/"); // Save images in the 'uploads' folder
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png|gif/;
+    const extName = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimeType = allowedTypes.test(file.mimetype);
+  
+    if (extName && mimeType) {
+      return cb(null, true);
+    } else {
+      cb(new Error("Only images are allowed!"));
+    }
+};
+
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // Limit: 5MB
+    fileFilter,
+});
+
+module.exports = upload;
