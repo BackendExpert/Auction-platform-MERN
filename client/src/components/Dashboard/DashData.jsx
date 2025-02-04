71aa4cb4eb1b1e3b97f365218691174346b6c550
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-04 10:12:42 +0530

    Create DashData.jsx

---
+++ client/src/components/Dashboard/DashData.jsx
@@ -0,0 +1,28 @@
+const admindata = [
+    {
+        id: 1,
+        name: 'User',
+        icon: '',
+        value: 500
+    },
+    {
+        id: 2,
+        name: 'Auction',
+        icon: '',
+        value: 500
+    },
+    {
+        id: 3,
+        name: 'Reported Auction',
+        icon: '',
+        value: 500
+    },
+    {
+        id: 4,
+        name: 'Pltform Income',
+        icon: '',
+        value: 500
+    },
+]
+
+export { admindata }
