2ee6080a096641b3f2b4a7d87dd75e814d6d777e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-13 10:20:53 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,17 @@ import ForgetPassword from "./pages/AuthPages/ForgetPassword";
 import PrivateRoute from "./components/Auth/PrivateRoute";
 import Dashboard from "./components/Dashboard/Dashboard";
 import DashHome from "./pages/DashBoard/DashHome";
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+=======
+import UserManagement from "./pages/Users/UserManagement";
+import ViewUser from "./pages/Users/ViewUser";
+import CreateAuction from "./pages/Auction/CreateAuction";
+>>>>>>> Stashed changes
+=======
+import UserManagement from "./pages/Users/UserManagement";
+import ViewUser from "./pages/Users/ViewUser";
+>>>>>>> 9ecb9ea7eb045f284a4b69714bb7b29530f9fb57
 
 export default function App() {
   const [showNavBar, setShowNavBar] = useState(true);
@@ -69,10 +80,12 @@ export default function App() {
         <Route path="/Dashboard/" element={ <PrivateRoute element={<Dashboard />} />} >
           <Route path="*" element={<PageNotFound /> } />
           <Route path="Home" element={<DashHome /> } />
-        </Route>
-
-
-        
+          <Route path="UserManagement" element={<UserManagement /> } />
+          <Route path="ViewUser/:id" element={<ViewUser /> } />
+          <Route path="CreateAuction" element={<CreateAuction /> } /> 
+          <Route path="UserManagement" element={<UserManagement /> } />
+          <Route path="ViewUser/:id" element={<ViewUser /> } />
+        </Route>        
       </Routes>
       {shouldShowFooter && <Footer />}
     </BrowserRouter>
