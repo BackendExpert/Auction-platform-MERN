d782ba7f54436c884a4879d6b7d5b7dcf7d6a66a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-13 10:36:08 +0530

    Update CreateAuction.jsx

--- client/src/pages/Auction/CreateAuction.jsx
+++ client/src/pages/Auction/CreateAuction.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import InputDefult from '../../components/Forms/InputDefult';
+import InputTextArea from '../../components/Forms/InputTextArea';
 
 const CreateAuction = () => {
     const [AuctionData, SetAuctionData] = useState({
@@ -45,8 +47,40 @@ const CreateAuction = () => {
         <h1 className="font-semibold text-xl text-[#FF5722] uppercase">Create Auction</h1>
         <hr className="my-4" />
 
-        <div className="">
-            <form onSubmit={headleSubmit} method="post"></form>
+        <div className="flex">
+            <div className="w-1/2 p-4">
+                <form onSubmit={headleSubmit} method="post">
+                    <div className="">
+                        <p className="text-[#FF5722]">Auction Title : </p>
+
+                        <div className="p-4">
+                            <InputDefult 
+                                type={'text'}
+                                name={'title'}
+                                value={AuctionData.title}
+                                required={true}
+                                placeholder={"Auction Title"}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="my-4">
+                        <p className="text-[#FF5722]">Auction Description : </p>
+
+                        <div className="p-4">
+                            <InputTextArea 
+                                name={'description'}
+                                value={AuctionData.description}
+                                required={true}
+                                placeholder={'Auction Description'}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                    </div>
+                </form>
+            </div>
+
         </div>
     </div>
 
